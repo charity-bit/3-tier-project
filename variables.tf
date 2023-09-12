e5025b8e4766043b153b3f6dc73a356f1e3d41e8
Charity Nyanchera <charitynyanchera@gmail.com>
2023-09-12 12:52:29 +0300

    added Networking and common variables

---
+++ variables.tf
@@ -0,0 +1,14 @@
+#Common Variables
+variable "region" {}
+variable "tags" {}
+variable "project" {}
+
+
+#Networking Variables
+variable "networking_connectivity_type" {}
+variable "networking_map_public_ip_on_launch" {}
+variable "networking_private_data_subnet_cidrs" {}
+variable "networking_private_subnet_cidrs" {}
+variable "networking_public_subnet_cidrs" {}
+variable "networking_vpc_cidr" {}
+
