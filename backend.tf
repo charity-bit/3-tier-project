6ab7470a93f1173d1e9be5664db2e70388b11b95
Charity Nyanchera <charitynyanchera@gmail.com>
2023-09-12 13:08:03 +0300

    added backend configs

---
+++ backend.tf
@@ -0,0 +1,41 @@
+#In Cases Where you are storing your state files remote
+# and you want your remote provider(terraform cloud or terraform enterprise) to manage your infra.
+
+
+#Storing in s3
+terraform {
+  backend "s3" {
+      bucket = "bucket-name"
+      key    = "state/terraform.tfstate"
+      region = "region-name"
+      dynamodb_table = "dynamodb-table"
+      encrypt = "true"
+  }
+}
+
+
+#Storing in terraform cloud
+terraform {
+
+    cloud {
+        organization = "your-organization-name"
+        workspaces {
+        name = "your-workspace-name"
+
+        }
+
+    }
+ }
+
+#Terraform Enterprise
+
+terraform {
+  cloud {
+    hostname = "your-host-name"
+    organization = "your-organization-name"
+    workspaces {
+      name = "workspace-name"
+    }
+  }
+
+}
